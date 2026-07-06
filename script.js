beec08726a80afeb61d6b94219e9a51c2ffcfba3
leonardorosaconceicao-byte <leonardo.rosa.conceicao@escola.pr.gov.br>
2026-07-06 11:52:20 -0300

    boton likes

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("curtidas")
+botao.addEventListener("click", aumentarcurtidas)
+
+function aumentarcurtidas(){
+  let curtida = document.querySelector("span")
+ curtida.textContent
+}
